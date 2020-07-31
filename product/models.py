38f8485bd63351253f776886323df30570b348d5
SaidjonPro <saidjon.xamidov92@gmail.com>
2020-07-31 22:33:08 +0500

    First Commit

---
+++ product/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+# Create your models here.
+from django.utils.safestring import mark_safe
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+class Category(MPTTModel):
+    STATUS = (
+        ('True', 'Mavjud'),
+        ('False', 'Mavjud Emas'),
+    )
+    parent = TreeForeignKey('self',
+                               blank=True,
+                               null=True,
+                               related_name='children',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=15, choices=STATUS)
+    slug = models.SlugField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Mavjud'),
+        ('False', 'Mavjud Emas'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, unique=True)
+    keywords = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    minamount = models.IntegerField()
+    detail = RichTextUploadingField()
+    status = models.CharField(max_length=15, choices=STATUS)
+    slug = models.SlugField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50">'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Downloads\myshop-master\myshop-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
